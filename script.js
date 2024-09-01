159a6f76fb98d88791b5294b596ab026da5742af
ElsonJuniorDev <108693146+ElsonJuniorDev@users.noreply.github.com>
2024-09-01 14:57:28 -0300

    -features seção home

--- script.js
+++ script.js
@@ -1,15 +1,41 @@
+//Menu hamburguer//
 const menuBtn = document.getElementById("menu-btn");
+
 const navLinks = document.getElementById("nav-links");
 const menuBtnicon = menuBtn.querySelector("i");
 
 menuBtn.addEventListener("click", (e) => {
-    navLinks.classList.toggle("open");
+  navLinks.classList.toggle("open");
 
-    const isOpen = navLinks.classList.contains("open");
-    menuBtnicon.setAttribute("class", isOpen ? "ri-close-line" : "ri-menu-line");
-}); 
+  const isOpen = navLinks.classList.contains("open");
+  menuBtnicon.setAttribute("class", isOpen ? "ri-close-line" : "ri-menu-line");
+});
 
-navLinks.addEventListener("click", (e) =>{
-    navLinks.classList.remove("open");
-    menuBtnicon.setAttribute("class", "ri-menu-line");
-})
+navLinks.addEventListener("click", (e) => {
+  navLinks.classList.remove("open");
+  menuBtnicon.setAttribute("class", "ri-menu-line");
+});
+
+//Animações - home//
+const scrollRevealOption = {
+  origin: "bottom",
+  distance: "50px",
+  dutarion: 1000,
+};
+
+ScrollReveal().reveal(".header_image img", {
+  ...scrollRevealOption,
+  origin: "right",
+});
+ScrollReveal().reveal(".header_content p", {
+  ...scrollRevealOption,
+  delay: 500,
+});
+ScrollReveal().reveal(".header_content h1", {
+  ...scrollRevealOption,
+  delay: 1000,
+});
+ScrollReveal().reveal(".header_btns", {
+  ...scrollRevealOption,
+  delay: 1500,
+});
